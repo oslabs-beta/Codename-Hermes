c9046a0ba26257c948d974c3abc7cb8dd9b60060
Mathew Hultquist <mathew@gates.services>
2023-02-23 22:26:38 -0500

    Initial boiler plate for the demo app

--- packages/demo_app/services/frontend/src/index.tsx
+++ packages/demo_app/services/frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-const App = () => <h1>Hello world!</h1>;
+const App = () => <h1>Hello world!1</h1>;
 
 createRoot(document.querySelector('#AppRoot')!).render(<App />);
