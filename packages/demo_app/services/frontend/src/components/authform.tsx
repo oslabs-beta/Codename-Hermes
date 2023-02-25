98f50df748a12e666be2ffa1cc2f6e1d1e7ce207
Jose Mencos <j.mencos56@gmail.com>
2023-02-25 16:27:13 -0500

    adds authform setup attempt

---
+++ packages/demo_app/services/frontend/src/components/authform.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+// import { Form, Link } from 'react-router-dom';
+
+const Button = () => (
+  <button>Found it</button>
+)
+
+export default Button;
--- packages/demo_app/services/frontend/src/index.tsx
+++ packages/demo_app/services/frontend/src/index.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import Button from './components/authform';
 
-const App = () => <h1>Hello world!1</h1>;
+const Root = () => (
+  <div>
+    <h1>Hello world!</h1>
+    <Button />
+  </div>
+);
 
-createRoot(document.querySelector('#AppRoot')!).render(<App />);
+createRoot(document.querySelector('#AppRoot')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Root />
+    </BrowserRouter>
+  </React.StrictMode>
+);
