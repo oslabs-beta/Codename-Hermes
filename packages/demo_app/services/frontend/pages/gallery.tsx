11680ca3213c28173f48ea777cf4f43c93bace56
Samantha <samanthaleighmills@gmail.com>
2023-02-25 16:27:21 -0500

    react router and basic pages

---
+++ packages/demo_app/services/frontend/pages/gallery.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Gallery = () => {
+  return <div></div>;
+};
+
+export default Gallery;
--- packages/demo_app/services/frontend/src/index.tsx
+++ packages/demo_app/services/frontend/src/index.tsx
@@ -1,6 +1,33 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Checkout from '../pages/checkout';
+import ImgBidding from '../pages/imgbidding';
+import UserPage from '../pages/userhomepage';
+import Login from '../pages/login';
+import Gallery from '../pages/gallery';
 
-const App = () => <h1>Hello world!1</h1>;
+const App = () => {
+  return (
+    <div>
+      <h1>Hello world!1</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/userpage' element={<UserPage />} />
+          <Route path='/gallery' element={<Gallery />} />
+          <Route path='/imgbidding' element={<ImgBidding />} />
+          <Route path='/checkout' element={<Checkout />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
 
-createRoot(document.querySelector('#AppRoot')!).render(<App />);
+createRoot(document.querySelector('#AppRoot')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
