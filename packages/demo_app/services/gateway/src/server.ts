084b665b6e4fd1c399ab2473360919688c7682c6
Mathew Hultquist <mathew@gates.services>
2023-03-01 22:52:10 -0500

    oops

--- packages/demo_app/services/gateway/src/server.ts
+++ packages/demo_app/services/gateway/src/server.ts
@@ -21,6 +21,7 @@ const consumer = new Consumer(
 try {
   app.get('/favicon.ico', (_, res) => res.status(404).send('No'));
 
+  // /james
   app.get('/:test', (req, res) => {
     const { test } = req.params;
     const secret = Math.random().toString(32).substring(3);
@@ -38,6 +39,24 @@ try {
       ],
       () => console.log(`Sent ${test} to the bidding service.`)
     );
+
+    // This will really be in a seperate "server" but acts like it's here
+    //   consumer.on('message', (message) => {
+    // const { msg, code } = JSON.parse(message.value as string) as {
+    //   msg: string;
+    //   code: string;
+    // };
+
+    // const newMsg = { response: `Hello, ${msg}!`, code };
+    // producer.send(
+    //   [
+    //     {
+    //       topic: 'gateway',
+    //       messages: JSON.stringify(newMsg),
+    //     },
+    //   ],
+    //   () => console.log(`Sent ${newMsg.response} to the gateway.`)
+    // );
 
     // When the gateway receives a response, send that to the client.
     // This is probobly really bad practice because this will accept any response even when there is no client waiting for a response.
