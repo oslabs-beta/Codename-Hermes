6f099de8be9bc64e67ab5a6f6fc4e04237886c71
H3R01A <aallen0001@gmail.com>
2023-03-04 18:10:29 -0500

    got streams working! Just need to refactor to not use files

--- packages/demo_app/services/bidding/src/service.ts
+++ packages/demo_app/services/bidding/src/service.ts
@@ -1,4 +1,6 @@
-import { KafkaClient, Producer, Consumer } from 'kafka-node';
+import { KafkaClient, Producer, Consumer, ProducerStream, ConsumerGroupStream, ConsumerGroup,KafkaClientOptions,ProducerStreamOptions, ConsumerGroupStreamOptions   } from 'kafka-node';
+const Transform = require('stream').Transform;
+const _ = require('lodash');
 import env from 'dotenv';
 env.config();
 
@@ -21,33 +23,93 @@ const client = new KafkaClient({
   kafkaHost: process.env.KAFKA_HOST,
 });
 
-const producer = new Producer(client);
-const consumer = new Consumer(
-  client,
-  [
-    {
-      topic: 'bidding',
-      offset: 2,
-    },
-  ],
-  { autoCommit: true }
-);
 
-producer.on('ready', () => console.log('Bidding service ready.'));
-consumer.on('message', (message) => {
-  const { msg, code } = JSON.parse(message.value as string) as {
-    msg: string;
-    code: string;
-  };
+const kafkaClient2: KafkaClientOptions = {
+  kafkaHost: process.env.KAFKA_HOST,
+};
 
-  const newMsg = { response: `Hello, ${msg}!`, code };
-  producer.send(
-    [
-      {
-        topic: 'gateway',
-        messages: JSON.stringify(newMsg),
-      },
-    ],
-    () => console.log(`Sent ${newMsg.response} to the gateway.`)
-  );
+const options: ProducerStreamOptions = {
+  kafkaClient: kafkaClient2,
+};
+
+const consumerOptions: ConsumerGroupStreamOptions = {
+  kafkaHost: process.env.KAFKA_HOST,
+  groupId: 'ExampleTestGroup',
+  sessionTimeout: 15000,
+  protocol: ['roundrobin'],
+  // asyncPush: false,
+  id: 'consumer1',
+  fromOffset: 'latest'
+}
+
+// const producer = new Producer(client);
+
+const producer = new ProducerStream(options);
+
+// const consumer = new Consumer(
+//   client,
+//   [
+//     {
+//       topic: 'bidding',
+//       offset: 2,
+//     },
+//   ],
+//   { autoCommit: true }
+// );
+
+
+const topicObj = [
+    'bidding'
+    ]
+
+/**
+ * creation of consumer with ConsumerGroupStream
+ */
+const consumerGroup = new ConsumerGroupStream(consumerOptions, topicObj);
+
+/**
+ * creation of ConsumerGroup
+ */
+const consumer = new ConsumerGroup(consumerOptions, topicObj);
+
+console.log('hello from bidding');
+console.log(`kafkaHost ${process.env.KAFKA_HOST}`)
+console.log(`kafkaClient ${kafkaClient2.kafkaHost}`)
+
+
+
+const messageTransform = new Transform({
+  objectMode: true,
+  decodeStrings: true,
+  transform (text:any, __:never, callback:any) {
+    console.log(`Received message ${text.value} transforming input`);
+    callback(null, {
+      topic: 'gateway',
+      messages: text.value
+    });
+  }
 });
+
+consumerGroup.pipe(messageTransform).pipe(producer);
+
+
+//producer.on('ready', () => console.log('Bidding service ready.'));
+
+// consumer.on('message', (message) => {
+//   const { msg, code } = JSON.parse(message.value as string) as {
+//     msg: string;
+//     code: string;
+//   };
+
+//   const newMsg = { response: `Hello, ${msg}!`, code };
+
+//   producer.sendPayload(
+//     [
+//       {
+//         topic: 'gateway',
+//         messages: JSON.stringify(newMsg),
+//       },
+//     ],
+//     () => console.log(`Sent ${newMsg.response} to the gateway.`)
+//   );
+// });
