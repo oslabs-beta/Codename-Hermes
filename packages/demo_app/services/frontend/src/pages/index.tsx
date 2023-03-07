c012cefa85911454d93e018a5c03ff3566650a19
Mathew Hultquist <mathew@gates.services>
2023-03-06 23:32:40 -0500

    Merged mjhul/docket-init with sam/jose branches

--- packages/demo_app/services/frontend/src/pages/index.tsx
+++ packages/demo_app/services/frontend/src/pages/index.tsx
@@ -1,35 +1,57 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import Image from 'next/image';
+import apirateslifeforme from '../../public/apirateslifeforme.png';
+import dotsoncardstock from '../../public/dotsoncardstock.png';
+import heart from '../../public/heart.png';
+import seaturtle from '../../public/seaturtle.png';
+import starrynightmixedmedia from '../../public/starrynightmixedmedia.png';
+import undertherainbow from '../../public/undertherainbow.png';
+import underwaterseascape from '../../public/underwaterseascape.png';
 
-type Count = {
-  count: number;
-};
+const artWork: any[] = [
+  apirateslifeforme,
+  dotsoncardstock,
+  heart,
+  seaturtle,
+  starrynightmixedmedia,
+  undertherainbow,
+  underwaterseascape,
+];
 
-export default function Main() {
-  const [count, setCount] = useState<number | null>(null);
-
-  useEffect(() => {
-    if (!count)
-      fetch('/api/count')
-        .then((d) => d.json())
-        .then((d: Count) => setCount(d.count));
-  });
-
-  const increment = () => {
-    fetch('/api/count', {
-      method: 'POST',
-    })
-      .then((d) => d.json())
-      .then((d: Count) => setCount(d.count));
-  };
-
+const Gallery = () => {
   return (
-    <div className='Main'>
-      <h1>Kafka counter test</h1>
+    <div className='flex flex-col'>
+      <h1 className='text-3xl text-cyan-800'>Welcome to CH Gallery</h1>
       <p>
-        Your current count is:{' '}
-        <span color='red'>{count ?? 'Waiting for count.'}</span>
+        This exhibit features a diverse array of works created with a variety of
+        mediums, including crayon, marker, pencil, and even flower petals. The
+        pieces on display showcase a range of subjects, from realistic
+        depictions of everyday life to whimsical images of fantasy and
+        imagination. Each piece in the collection is a testament to the
+        creativity and skill of its artist.
       </p>
-      <button onClick={increment}>Increment Counter</button>
+      <p>
+        {' '}
+        The crayon drawings evoke a sense of childhood nostalgia, with bright,
+        bold colors and simple yet charming compositions. the collection also
+        includes pieces created with unconventional materials, such as flower
+        petals. These works incorporate natural elements in a way that is both
+        beautiful and thought-provoking.
+      </p>
+      <p>
+        {' '}
+        Whether the images are based on real life or the imagination, each piece
+        in this exhibit is a unique expression of the {"artist's"} vision. We
+        invite you to take a closer look and discover the beauty and diversity
+        of these wonderful works of art.
+      </p>
+      <div className='galDisp'>
+        {artWork.map((img, index) => (
+          <Image src={img} key={index} className='galImg' alt={"Mabel's art"} />
+        ))}
+      </div>
     </div>
   );
-}
+};
+
+export default Gallery;
---
+++ packages/demo_app/services/frontend/src/types/images.d.ts
@@ -0,0 +1,24 @@
+declare module '*.jpg' {
+  const value: string;
+  export = value;
+}
+
+declare module '*.jpeg' {
+  const value: string;
+  export = value;
+}
+
+declare module '*.png' {
+  const value: string;
+  export = value;
+}
+
+declare module '*.webp' {
+  const value: string;
+  export = value;
+}
+
+declare module '*.svg' {
+  const value: string;
+  export = value;
+}
