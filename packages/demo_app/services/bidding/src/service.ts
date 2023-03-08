0d3bb5efcbe0ca2873b8582b2cfe01f41c6fb396
H3R01A <aallen0001@gmail.com>
2023-03-08 00:43:08 -0500

    added template for bidding service backend

--- packages/demo_app/services/bidding/src/service.ts
+++ packages/demo_app/services/bidding/src/service.ts
@@ -23,6 +23,23 @@ const consumer = new Consumer(
 
 let count = 0;
 
+/**
+ * Bid interface
+ * 
+ */
+
+interface Bid {
+  galleryID: string,
+  currentBid: number,
+  postBid: number,
+
+}
+
+/**
+ * KafkaData 
+ * 
+ */
+
 type KafkaData = {
   code: string;
   method: string;
@@ -31,19 +48,48 @@ type KafkaData = {
 producer.on('ready', () => console.log('Bidding service ready.'));
 
 consumer.on('message', (message) => {
+
   const { code, method } = JSON.parse(message.value as string) as KafkaData;
+
+  const {galleryID, currentBid, postBid } = JSON.parse(message.value as string) as Bid;
 
   console.log(`Received ${method ?? 'no method'} with code ${code}`);
 
+  let newBid;
+  //Handle Bid incrementing
+  if(method === 'POST'){
+
+    newBid = currentBid + postBid;
+  }
+
+  //original POST method
   if (method === 'POST') count += 1;
 
   producer.send(
     [
       {
         topic: 'gateway',
-        messages: JSON.stringify({ code, count }),
+        messages: JSON.stringify({ galleryID, newBid  }),
       },
     ],
     () => console.log(`Sent ${count} to the gateway.`)
   );
+
+
+  /**
+   * Original send producer method
+   */
+
+  // producer.send(
+  //   [
+  //     {
+  //       topic: 'gateway',
+  //       messages: JSON.stringify({ code, count }),
+  //     },
+  //   ],
+  //   () => console.log(`Sent ${count} to the gateway.`)
+  // );
+
+
+
 });
