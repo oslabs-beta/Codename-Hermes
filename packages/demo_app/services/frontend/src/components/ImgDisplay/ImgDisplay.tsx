81ad4e3e0b0cadc98ba2ca501462272468911b6a
Jose Mencos <j.mencos56@gmail.com>
2023-03-10 22:55:25 -0500

    sams gallery added

--- packages/demo_app/services/frontend/src/components/ImgDisplay/ImgDisplay.tsx
+++ packages/demo_app/services/frontend/src/components/ImgDisplay/ImgDisplay.tsx
@@ -1,32 +1,42 @@
-import React from 'react';
+import React, { FC, useState } from 'react';
 import Image from 'next/image';
 import styles from './imgstyledisplay.module.scss';
 
-const ImgDisplay = () => {
-  const imgArr = [
-    'https://www.boredpanda.com/blog/wp-content/uploads/2022/09/relatable-funny-memes-22-63284d45ebe28__700.jpg',
-    'https://thumbor.bigedition.com/dog-with-caffeine/jjfqING-CYZnbfuDFyFTE6oEEHg=/301x145:888x732/800x800/filters:quality(80)/granite-web-prod/4e/9d/4e9d7d8aac824532b6ffb2b9ffcee8c3.jpeg',
-    'https://assets-global.website-files.com/5f3c19f18169b62a0d0bf387/60d33bf3a2121cc74f72a286_0AEnnZYBAYzmSXzyGaopjvEGKKO1yFqf_zMXqFTAT6vUDfXVPRifI3J8oBjZUl51PnRQqf2tqpjiRt33IKgg4sTiJi2YShGTN5iNUIoYbNj2HXobiQg4-k7yBiNT54EKOGYyZOR2.png',
-  ];
+const ImgDisplay : FC<{imgsrc:string, id:number}> = ({imgsrc,id}) => {
+
+const [currBid, setCurrBid] = useState(0);
+const [newBid, setNewBid ] = useState(0)
+
+const handleClick = async () => {
+  const response = await fetch('/api/bidRoute', {
+    method: 'POST',
+    body: JSON.stringify({currBid,id, newBid }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  const data = await response.json();
+}
+  
 
   return (
     <div className={styles.imgdisplay}>
-      {imgArr.map((el, i) => {
-        return (
-          <div key={i + 2}>
-            <img
-              key={i}
-              src={el}
-              width='200px'
+      
+        
+          <div >
+            <Image
+              src={imgsrc}
+              // width='200px'
               alt=''
             />
             <br/>
-            <input type='text' placeholder='enter bid'></input>
+            <p>Current Bid: ${currBid}</p>
+          
+            <input type='number' placeholder='enter bid'></input>
             <br/>
-            <button>Submit bud</button>
+            <button onClick={handleClick}>Submit bid</button>
           </div>
-        );
-      })}
+        
     </div>
   );
 };
