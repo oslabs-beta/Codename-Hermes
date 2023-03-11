41f1dae3a6167293de7f20158bf4366e83b05d58
Mathew Hultquist <mathew@gates.services>
2023-03-11 15:41:45 -0500

    Fixed semicolin

--- packages/demo_app/services/frontend/src/pages/_app.tsx
+++ packages/demo_app/services/frontend/src/pages/_app.tsx
@@ -8,7 +8,7 @@ const cinzel = Cinzel({ weight: '500', subsets: ['latin'] });
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={poppins.className}>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </div>
   );
 }
--- packages/library/lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-import Kafka from './brokers/kafka/main';
-declare const CodenameHermes: {
-    kafka: typeof Kafka;
-    rabbitmq: string;
-    redit: string;
-};
-export default CodenameHermes;
