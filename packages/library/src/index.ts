92b71d152600a7045d69cc19ff3a4d4b3fb08a03
Mathew Hultquist <mathew@gates.services>
2023-04-15 12:56:54 -0400

    Minor refactor

--- packages/library/src/index.ts
+++ packages/library/src/index.ts
@@ -7,9 +7,9 @@ import Kafka, {
 } from './brokers/kafka/main';
 
 const CodenameHermes = {
-  kafka: Kafka,
-  rabbitmq: 'Not implemented.',
-  redit: 'Not implemented.',
+  Kafka,
+  Rabbit: 'Not implemented.',
+  Redis: 'Not implemented.',
 };
 
 export default CodenameHermes;
