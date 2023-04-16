959b11801277ee131f91c182d1d0456d6eaee04d
Mathew Hultquist <mathew@gates.services>
2023-04-15 20:00:24 -0400

    Basic layout for rabbit class

--- packages/library/src/brokers/rabbit/main.ts
+++ packages/library/src/brokers/rabbit/main.ts
@@ -1,1 +1,33 @@
-export default class Rabbit = 
+import MessageBroker, {
+  GenericClientOptions,
+  GenericListenerOptions,
+  GenericMessage,
+  GenericTopic,
+  MessageCallback,
+} from '../MessageBroker';
+
+export type RabbitClientOptions = GenericClientOptions;
+
+// TODO: add the rest of the options
+export type RabbitTopic = GenericTopic<{
+  exclusive?: boolean;
+  durable?: boolean;
+  autoDelete?: boolean;
+  messageTtl?: number;
+}>;
+
+export type RabbitListenerOptions = GenericListenerOptions & {};
+
+export type RabbitMessage = GenericMessage & {};
+
+export default class Rabbit extends MessageBroker {
+  constructor(connection: RabbitClientOptions, topics: RabbitTopic) {
+    super(connection, topics);
+  }
+
+  send(topic: string, message: string | string[]) {}
+
+  listener(topics: string[], options: RabbitListenerOptions) {}
+
+  consume(topic: string, callback: MessageCallback<RabbitMessage>) {}
+}
